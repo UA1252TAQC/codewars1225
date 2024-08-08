a125de18f830a87059dd67f2dc5c505799b53666
ol271176 <ol271176@gmail.com>
2024-08-08 19:49:24 +0300

    add balance, haÑÑs me bugags

--- src/main/java/org/academy/kata/implementation/ol271176/SixImpl.java
+++ src/main/java/org/academy/kata/implementation/ol271176/SixImpl.java
@@ -10,7 +10,37 @@ public class SixImpl implements Six {
 
     @Override
     public String balance(String book) {
-        return "";
+        // delete empty lines
+        String[] lines = book.split("\n");
+        StringBuilder result = new StringBuilder();
+
+        //find balance
+        double balance = Double.parseDouble(lines[0].replaceAll("[^0-9.]", ""));
+        result.append(String.format("Original_Balance: %.2f%s", balance, System.lineSeparator()));
+        double totalExpense = 0, avgExpense = 0, count = 0;
+
+        //process each line
+        for(int i=1; i<lines.length; i++){
+            String proccessedLine = lines[i].replaceAll("[^a-zA-Z0-9. ]", "");
+
+            if(proccessedLine.isEmpty()) continue;
+
+            String[] info = proccessedLine.split(" ");
+            int checkNumber = Integer.parseInt(info[0]);
+            String category = info[1];
+            double checkAmount = Double.parseDouble(info[2]);
+            double totalAmount = balance - checkAmount;
+            balance -= checkAmount;
+
+            totalExpense += checkAmount;
+            count++;
+
+            result.append(String.format("%d %s %.2f Balance %.2f%s", checkNumber, category, checkAmount, balance, System.lineSeparator()));
+        }
+        result.append(String.format("Total expense  %.2f%s", totalExpense, System.lineSeparator()));
+        result.append(String.format("Average expense  %.2f", totalExpense/count));
+
+        return result.toString();
     }
 
     @Override
