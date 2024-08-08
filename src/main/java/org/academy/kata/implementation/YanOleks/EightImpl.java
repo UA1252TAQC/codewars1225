e64b20ddc4bb367cc4de237fa7a9cf71299b2303
YanOleks <yanytskyi.oleksandr@gmail.com>
2024-08-08 22:12:40 +0300

    squareOrSquareRoot

--- src/main/java/org/academy/kata/implementation/YanOleks/EightImpl.java
+++ src/main/java/org/academy/kata/implementation/YanOleks/EightImpl.java
@@ -20,7 +20,15 @@ public class EightImpl implements Eight {
 
     @Override
     public int[] squareOrSquareRoot(int[] array) {
-        return new int[0];
+        for (int i = 0; i < array.length; i++){
+            double root = Math.sqrt(array[i]);
+            if (root == (int)root){
+                array[i] = (int)root;
+            } else {
+                array[i] *= array[i];
+            }
+        }
+        return array;
     }
 
     @Override
