f2993d627b83824b4c619194aa954e122f36c103
ol271176 <ol271176@gmail.com>
2024-08-08 18:04:36 +0300

    add newAvg

--- src/main/java/org/academy/kata/implementation/ol271176/SevenImpl.java
+++ src/main/java/org/academy/kata/implementation/ol271176/SevenImpl.java
@@ -5,7 +5,16 @@ import org.academy.kata.Seven;
 public class SevenImpl implements Seven {
     @Override
     public long newAvg(double[] arr, double navg) {
-        return 0;
+        double arrSum = 0;
+        for(double e : arr){
+            arrSum += e;
+        }
+        long result = (long) Math.ceil(navg*(arr.length+1) - arrSum);
+        if(result > 0){
+            return result;
+        }else{
+            throw new IllegalArgumentException();
+        }
     }
 
     @Override
