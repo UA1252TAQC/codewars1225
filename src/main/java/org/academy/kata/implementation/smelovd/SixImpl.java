254daa03ca966296c8b8abb86945206d28e472e4
Denys Smielov <smelov.denis.2006@gmail.com>
2024-08-08 21:01:18 +0300

    Easy Balance Checking

--- src/main/java/org/academy/kata/implementation/smelovd/SixImpl.java
+++ src/main/java/org/academy/kata/implementation/smelovd/SixImpl.java
@@ -10,7 +10,37 @@ public class SixImpl implements Six {
 
     @Override
     public String balance(String book) {
-        return "";
+        if (book == null || book.trim().isEmpty()) {
+            throw new IllegalArgumentException();
+        }
+
+        final StringBuilder builder = new StringBuilder();
+        final String[] clearLines = book.replaceAll("[^\r\n a-zA-Z0-9.]", "").trim().split("\n");
+
+        final double finalOriginalBalance = Double.parseDouble(clearLines[0]);
+        double originalBalance = finalOriginalBalance;
+        builder.append("Original Balance: ").append(clearLines[0]);
+        for (int i = 1; i < clearLines.length; i++) {
+            while (clearLines[i].contains("  ")) {
+                clearLines[i] = clearLines[i].replaceAll("  ", " ");
+            }
+            String[] lineVariables = clearLines[i].split(" ");
+            builder.append("\\r\\n").append(clearLines[i].trim());
+
+            originalBalance = Math.round((originalBalance - Double.parseDouble(lineVariables[2])) * 100.0) / 100.0;
+            builder.append(" Balance ").append(String.format("%.2f", originalBalance));
+        }
+
+        this.setExpenses(builder, finalOriginalBalance, originalBalance, clearLines.length - 1);
+        return builder.toString();
+    }
+
+    private void setExpenses(StringBuilder builder, double finalOriginalBalance, double originalBalance, int count) {
+        double totalExpense = Math.round((finalOriginalBalance - originalBalance) * 100.0) / 100.0;
+        builder.append("\\r\\nTotal expense  ").append(String.format("%.2f", totalExpense));
+
+        double averageExpense = Math.round(totalExpense / count * 100.0) / 100.0;
+        builder.append("\\r\\nAverage expense  ").append(String.format("%.2f", averageExpense));
     }
 
     @Override
