b441a67fc3da6ebe70c2356d2964bb37220d987b
Vladyslava-Nezhnova <vldnezhnova@gmail.com>
2024-08-09 16:32:14 +0300

    solve newAvg task

--- src/main/java/org/academy/kata/implementation/VladyslavaNezhnova/SevenImpl.java
+++ src/main/java/org/academy/kata/implementation/VladyslavaNezhnova/SevenImpl.java
@@ -5,7 +5,18 @@ import org.academy.kata.Seven;
 public class SevenImpl implements Seven {
     @Override
     public long newAvg(double[] arr, double navg) {
-        return 0;
+        double currentSum = 0;
+        for (double donation : arr) {
+            currentSum += donation;
+        }
+        double requiredSum = navg * (arr.length + 1);
+        double newDonation = requiredSum - currentSum;
+
+        if (newDonation <= 0) {
+            throw new IllegalArgumentException("New donation must be positive");
+        }
+
+        return (long) Math.ceil(newDonation);
     }
 
     @Override
