1663b05f6a8af481d295376739975ebc233ae6dd
YanOleks <yanytskyi.oleksandr@gmail.com>
2024-08-09 17:28:37 +0300

    findNb

--- src/main/java/org/academy/kata/implementation/YanOleks/SixImpl.java
+++ src/main/java/org/academy/kata/implementation/YanOleks/SixImpl.java
@@ -5,7 +5,14 @@ import org.academy.kata.Six;
 public class SixImpl implements Six {
     @Override
     public long findNb(long m) {
-        return 0;
+        long numberOfCubes = 0;
+        long volume = 0;
+        while (volume < m){
+            numberOfCubes++;
+            volume += numberOfCubes * numberOfCubes * numberOfCubes;
+        }
+        if (volume == m) return numberOfCubes;
+        else return -1L;
     }
 
     @Override
