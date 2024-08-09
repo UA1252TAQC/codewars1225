a217fa1730cb5770b3ffd14efc93a0db854c19d4
Denys Smielov <smelov.denis.2006@gmail.com>
2024-08-10 01:20:30 +0300

    Rainfall

--- src/main/java/org/academy/kata/implementation/smelovd/SixImpl.java
+++ src/main/java/org/academy/kata/implementation/smelovd/SixImpl.java
@@ -2,6 +2,9 @@ package org.academy.kata.implementation.smelovd;
 
 import org.academy.kata.Six;
 
+import java.util.Arrays;
+import java.util.stream.DoubleStream;
+
 public class SixImpl implements Six {
     @Override
     public long findNb(long m) {
@@ -20,12 +23,22 @@ public class SixImpl implements Six {
 
     @Override
     public double mean(String town, String strng) {
-        return 0;
+        return getStream(town, strng).average().orElse(-1);
     }
 
     @Override
     public double variance(String town, String strng) {
-        return 0;
+        final double avg = mean(town, strng);
+        return getStream(town, strng).map(i -> Math.pow(avg - i, 2)).average().orElse(-1);
+    }
+
+    private DoubleStream getStream(String town, String strng) {
+        return Arrays.stream(strng.split("\n"))
+                .filter(townData -> townData.startsWith(town + ":"))
+                .map(townData -> townData.substring(town.length() + 1))
+                .flatMapToDouble(townData -> Arrays.stream(townData.split(","))
+                        .map(monthData -> monthData.split(" ")[1])
+                        .mapToDouble(Double::parseDouble));
     }
 
     @Override
