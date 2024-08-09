47497c0db16769e05a1a3ebe65770e8bfa9cbfbd
YanOleks <yanytskyi.oleksandr@gmail.com>
2024-08-09 10:57:36 +0300

    TwoDecimalPlaces

--- src/main/java/org/academy/kata/implementation/YanOleks/EightImpl.java
+++ src/main/java/org/academy/kata/implementation/YanOleks/EightImpl.java
@@ -35,7 +35,7 @@ public class EightImpl implements Eight {
 
     @Override
     public double TwoDecimalPlaces(double number) {
-        return 0;
+        return Math.round(number * 100) / 100.;
     }
 
     @Override
