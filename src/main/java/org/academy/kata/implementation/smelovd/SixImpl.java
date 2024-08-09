ffb3a7af4b9f51fd0fa076c78c7aecbde47ba9d6
Denys Smielov <smelov.denis.2006@gmail.com>
2024-08-10 01:40:54 +0300

    Ranking NBA

--- src/main/java/org/academy/kata/implementation/smelovd/SixImpl.java
+++ src/main/java/org/academy/kata/implementation/smelovd/SixImpl.java
@@ -30,7 +30,52 @@ public class SixImpl implements Six {
 
     @Override
     public String nbaCup(String resultSheet, String toFind) {
-        return "";
+        if (toFind.isEmpty()) return "";
+        int wins = 0, draws = 0, losses = 0, totalScored = 0, totalConceded = 0;
+
+        for (String match : resultSheet.split(",")) {
+            String[] teams = match.split("(?<=\\d)\\s+");
+            if (!isMatchMember(teams, toFind)) continue;
+
+            try {
+                int[] scores = extractScores(teams, toFind);
+                int scored = scores[0], conceded = scores[1];
+
+                if (scored > conceded) wins++;
+                else if (scored == conceded) draws++;
+                else losses++;
+
+                totalScored += scored;
+                totalConceded += conceded;
+            } catch (Exception e) {
+                return "Error(float number):" + match;
+            }
+        }
+
+        if (wins + draws + losses == 0) return String.format("%s:This team didn't play!", toFind);
+        return String.format("%s:W=%d;D=%d;L=%d;Scored=%d;Conceded=%d;Points=%d",
+                toFind, wins, draws, losses, totalScored, totalConceded, (wins * 3) + draws);
+    }
+
+    private boolean isMatchMember(String[] teams, String team) {
+        return teams[0].substring(0, teams[0].trim().lastIndexOf(" ")).trim().equals(team) ||
+                teams[1].substring(0, teams[1].trim().lastIndexOf(" ")).trim().equals(team);
+    }
+
+    private int[] extractScores(String[] teams, String team) {
+        final String team1 = teams[0].trim();
+        final String team2 = teams[1].trim();
+
+        if (isRequiredTeam(team, team1)) return new int[] {extractScore(team1), extractScore(team2)};
+        return new int[] {extractScore(team2), extractScore(team1)};
+    }
+
+    private boolean isRequiredTeam(String team, String team1) {
+        return team1.substring(0, team1.lastIndexOf(" ")).equals(team);
+    }
+
+    private int extractScore(String team) {
+        return Integer.parseInt(team.substring(team.lastIndexOf(" ")).trim());
     }
 
     @Override
