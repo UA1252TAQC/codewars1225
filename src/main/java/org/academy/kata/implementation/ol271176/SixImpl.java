c09894ffaf52d51f82a3917b1fa99c1ac4ae7b7e
ol271176 <ol271176@gmail.com>
2024-08-10 12:49:44 +0300

    add Rainfall

--- src/main/java/org/academy/kata/implementation/ol271176/SixImpl.java
+++ src/main/java/org/academy/kata/implementation/ol271176/SixImpl.java
@@ -20,12 +20,63 @@ public class SixImpl implements Six {
 
     @Override
     public double mean(String town, String strng) {
-        return 0;
+        String[] cities = strng.split("\n");
+        String townLine = null;
+        for (String cityLine : cities) {
+            String[] parts = cityLine.split(":");
+            String city = parts[0];
+            if (city.equals(town)) {
+                townLine = parts[1];
+                break;
+            }
+        }
+
+        if (townLine == null) {
+            return -1.0;
+        }
+
+        String[] townRecordsLine = townLine.replaceAll("[^0-9. ]", "").trim().split(" ");
+        double[] townRecords = new double[townRecordsLine.length];
+        for (int i = 0; i < townRecordsLine.length; i++) {
+            townRecords[i] = Double.parseDouble(townRecordsLine[i]);
+        }
+
+        double sum = 0;
+        for (double e : townRecords) {
+            sum += e;
+        }
+
+        return sum / townRecords.length;
     }
 
     @Override
     public double variance(String town, String strng) {
-        return 0;
+        String[] cities = strng.split("\n");
+        String townLine = null;
+        for (String cityLine : cities) {
+            String[] parts = cityLine.split(":");
+            String city = parts[0];
+            if (city.equals(town)) {
+                townLine = parts[1];
+                break;
+            }
+        }
+
+        if (townLine == null) return -1.0;
+
+        String[] townRecordsLine = townLine.replaceAll("[^0-9. ]", "").trim().split(" ");
+        double[] townRecords = new double[townRecordsLine.length];
+        for (int i = 0; i < townRecordsLine.length; i++) {
+            townRecords[i] = Double.parseDouble(townRecordsLine[i]);
+        }
+
+        double mean = mean(town, strng);
+        double sumOfSquares = 0;
+        for (double e : townRecords) {
+            sumOfSquares += (e - mean) * (e - mean);
+        }
+
+        return sumOfSquares / townRecords.length;
     }
 
     @Override
