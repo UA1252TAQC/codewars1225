b3d73269ed27d157977da2350305aeb2a4743d63
ol271176 <ol271176@gmail.com>
2024-08-10 12:55:28 +0300

    add Help the bookseller!

--- src/main/java/org/academy/kata/implementation/ol271176/SixImpl.java
+++ src/main/java/org/academy/kata/implementation/ol271176/SixImpl.java
@@ -1,5 +1,9 @@
 package org.academy.kata.implementation.ol271176;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import org.academy.kata.Six;
 
 public class SixImpl implements Six {
@@ -35,6 +39,29 @@ public class SixImpl implements Six {
 
     @Override
     public String stockSummary(String[] lstOfArt, String[] lstOf1stLetter) {
-        return "";
+        Map<Character, Integer> mapOfBooks = new LinkedHashMap<>();
+        for(String letter : lstOf1stLetter) mapOfBooks.put(letter.charAt(0), 0);
+
+        Pattern pattern = Pattern.compile("(\\D+)\\s(\\d+)");
+        for(String bookInfo : lstOfArt){
+            Matcher matcher = pattern.matcher(bookInfo);
+            if(matcher.find()){
+                char key = matcher.group(1).charAt(0);
+                int value = Integer.parseInt(matcher.group(2));
+                if(mapOfBooks.containsKey(key)){
+                    mapOfBooks.put(key, mapOfBooks.get(key) + value);
+                } else {
+                    continue;
+                }
+            }
+        }
+        StringBuffer sb = new StringBuffer();
+        for(Map.Entry<Character, Integer> entry : mapOfBooks.entrySet()){
+            if (sb.length() > 0) sb.append(" - ");
+            sb.append(String.format("(%c : %d)", entry.getKey(), entry.getValue()));
+
+        }
+
+        return sb.toString();
     }
 }
