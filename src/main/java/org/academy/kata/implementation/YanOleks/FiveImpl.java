2859dc5a7bb9fc9b0e9209bcc6a8cfb0ea96f77f
YanOleks <yanytskyi.oleksandr@gmail.com>
2024-08-11 21:56:52 +0300

    smallest

--- src/main/java/org/academy/kata/implementation/YanOleks/FiveImpl.java
+++ src/main/java/org/academy/kata/implementation/YanOleks/FiveImpl.java
@@ -27,6 +27,31 @@ public class FiveImpl implements Five {
 
     @Override
     public long[] smallest(long n) {
-        return new long[0];
+        String numStr = Long.toString(n);
+        int len = numStr.length();
+        int iMin = 0, jMin = 0;
+        String smallest = numStr;
+        for (int i = 0; i < len; i++) {
+            char currentChar = numStr.charAt(i);
+            StringBuilder sb = new StringBuilder(numStr);
+            sb.deleteCharAt(i);
+
+            for (int j = 0; j <= len - 1; j++) {
+                StringBuilder newSb = new StringBuilder(sb);
+                newSb.insert(j, currentChar);
+
+                String newValStr = newSb.toString();
+
+                if (newValStr.compareTo(smallest) < 0 ||
+                        (newValStr.equals(smallest) && i < iMin)) {
+                    smallest = newValStr;
+                    iMin = i;
+                    jMin = j;
+                }
+            }
+        }
+
+        long minVal = Long.parseLong(smallest);
+        return new long[] { minVal, iMin, jMin };
     }
 }
