95b792d072037ef7dcb9b882e3c971e741d89f03
Denys Smielov <smelov.denis.2006@gmail.com>
2024-08-11 04:05:00 +0300

    Find the smallest

--- src/main/java/org/academy/kata/implementation/smelovd/FiveImpl.java
+++ src/main/java/org/academy/kata/implementation/smelovd/FiveImpl.java
@@ -27,6 +27,28 @@ public class FiveImpl implements Five {
 
     @Override
     public long[] smallest(long n) {
-        return new long[0];
+        final String s = Long.toString(n);
+        long minNumber = n;
+        int fromIndex = -1, toIndex = -1;
+
+        for (int i = 0; i < s.length(); i++) {
+            final String numWithoutFromIndex = s.substring(0, i) + s.substring(i + 1);
+
+            for (int j = 0; j <= numWithoutFromIndex.length(); j++) {
+                final long newNumber = Long.parseLong(numWithoutFromIndex.substring(0, j) + s.charAt(i) + numWithoutFromIndex.substring(j));
+
+                if (newNumber < minNumber || (newNumber == minNumber && isIndexSmaller(fromIndex, toIndex, i, j))) {
+                    minNumber = newNumber;
+                    fromIndex = i;
+                    toIndex = j;
+                }
+            }
+        }
+
+        return new long[] { minNumber, fromIndex, toIndex };
+    }
+
+    private boolean isIndexSmaller(int fromIndex, int toIndex, int i, int j) {
+        return i < fromIndex || (i == fromIndex && j < toIndex);
     }
 }
