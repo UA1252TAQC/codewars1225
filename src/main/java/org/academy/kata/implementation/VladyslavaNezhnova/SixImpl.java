aa54cd7c2b8ed763d7c28af083edba04655a956d
Vladyslava <vldnezhnova@gmail.com>
2024-08-12 13:39:46 +0300

    solve balance task

--- src/main/java/org/academy/kata/implementation/VladyslavaNezhnova/SixImpl.java
+++ src/main/java/org/academy/kata/implementation/VladyslavaNezhnova/SixImpl.java
@@ -10,7 +10,32 @@ public class SixImpl implements Six {
 
     @Override
     public String balance(String book) {
-        return "";
+        book = book.replaceAll("[^a-zA-Z0-9.\\s]", "");
+
+        String[] lines = book.split("\\n+");
+        double originalBalance = Double.parseDouble(lines[0]);
+        double currentBalance = originalBalance;
+        double totalExpense = 0.0;
+
+        StringBuilder report = new StringBuilder();
+        report.append("Original Balance: ").append(String.format("%.2f", originalBalance)).append("\\r\\n");
+
+
+        for (int i = 1; i < lines.length; i++) {
+            String[] parts = lines[i].split("\\s+");
+            double expense = Double.parseDouble(parts[2]);
+            currentBalance -= expense;
+            totalExpense += expense;
+
+            report.append(parts[0]).append(" ").append(parts[1]).append(" ")
+                    .append(String.format("%.2f", expense)).append(" Balance ")
+                    .append(String.format("%.2f", currentBalance)).append("\\r\\n");
+        }
+
+        report.append("Total expense  ").append(String.format("%.2f", totalExpense)).append("\\r\\n");
+        report.append("Average expense  ").append(String.format("%.2f", totalExpense / (lines.length - 1)));
+
+        return report.toString();
     }
 
     @Override
