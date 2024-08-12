16b64595798311fc15658241fe21c014fd0f49bb
Maria Marchenko <maria.dmit.rovna.slv@gmail.com>
2024-08-12 10:28:47 +0300

    add seriesSum method realization

--- src/main/java/org/academy/kata/implementation/marchenko2005/SevenImpl.java
+++ src/main/java/org/academy/kata/implementation/marchenko2005/SevenImpl.java
@@ -1,6 +1,9 @@
 package org.academy.kata.implementation.marchenko2005;
 
 import org.academy.kata.Seven;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
 
 public class SevenImpl implements Seven {
     @Override
@@ -10,6 +13,13 @@ public class SevenImpl implements Seven {
 
     @Override
     public String seriesSum(int n) {
-        return "";
+        BigDecimal res = new BigDecimal("0.0");
+        for (int i = 0; i < n; i++) {
+            BigDecimal term = BigDecimal.valueOf(1.0 / (1 + 3 * i));
+            res = res.add(term);
+        }
+        res = res.setScale(2, RoundingMode.HALF_UP);
+
+        return res.toString();
     }
 }
