6fc10b3172d7f6a1ad61692d6c0cc655a6dbc217
Maria Marchenko <maria.dmit.rovna.slv@gmail.com>
2024-08-12 10:50:12 +0300

    add findNb method realization

--- src/main/java/org/academy/kata/implementation/marchenko2005/SixImpl.java
+++ src/main/java/org/academy/kata/implementation/marchenko2005/SixImpl.java
@@ -5,7 +5,15 @@ import org.academy.kata.Six;
 public class SixImpl implements Six {
     @Override
     public long findNb(long m) {
-        return 0;
+        long n = 0;
+        long totalVolume = 0;
+
+        while (totalVolume < m) {
+            n++;
+            totalVolume += n * n * n;
+        }
+
+        return totalVolume == m ? n : -1;
     }
 
     @Override
