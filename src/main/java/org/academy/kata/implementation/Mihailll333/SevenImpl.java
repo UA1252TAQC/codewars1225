0a8382fc5fc40f0365af0b9c25d4cc558ccd6fa1
mihail <turubarovvvv@gmail.com>
2024-08-13 22:34:56 +0300

    newAvg

--- src/main/java/org/academy/kata/implementation/Mihailll333/SevenImpl.java
+++ src/main/java/org/academy/kata/implementation/Mihailll333/SevenImpl.java
@@ -5,7 +5,17 @@ import org.academy.kata.Seven;
 public class SevenImpl implements Seven {
     @Override
     public long newAvg(double[] arr, double navg) {
-        return 0;
+        long sumArr = 0;
+        for (double donation : arr) {
+            sumArr += (long) donation;
+        }
+        int n = arr.length;
+        long newDonation = (long) Math.ceil(navg * (n + 1) - sumArr);
+
+        if (newDonation <= 0) {
+            throw new IllegalArgumentException("Invalid donation amount");
+        }
+        return (int) newDonation;
     }
 
     @Override
