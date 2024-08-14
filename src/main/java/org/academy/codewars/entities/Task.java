194d1fc462da62a53689de12cc24ec3fd64d29cc
AndriyVel05 <andriy.velychko.work@gmail.com>
2024-08-14 17:08:38 +0300

    Tasks_Enum

--- src/main/java/org/academy/codewars/entities/Task.java
+++ src/main/java/org/academy/codewars/entities/Task.java
@@ -4,22 +4,20 @@ import java.util.List;
 import java.util.function.Supplier;
 
 public enum Task {
-    TASK1("Title1", "Description1", () -> new Object(), List.of("Param1"), "Task1"),
-    TASK2("Title2", "Description2", () -> new Object(), List.of("Param2"), "Task2"),
-    TASK3("Title3", "Description3", () -> new Object(), List.of("Param3"), "Task3");
+    TASK1("Title1", "Description1", () -> new Object(), List.of("Param1"),
+            TASK2("Title2", "Description2", () -> new Object(), List.of("Param2"),
+                    TASK3("Title3", "Description3", () -> new Object(), List.of("Param3");
 
     private final String title;
     private final String description;
     private final Supplier<Object> supplier;
     private final List<String> requiredParams;
-    private final String task;
 
-    Task(String title, String description, Supplier<Object> supplier, List<String> requiredParams, String task) {
+    public Task(String title, String description, Supplier<Object> supplier, List<String> requiredParams) {
         this.title = title;
         this.description = description;
         this.supplier = supplier;
         this.requiredParams = requiredParams;
-        this.task = task;
     }
 
     public String getTitle() {
@@ -37,8 +35,3 @@ public enum Task {
     public List<String> getRequiredParams() {
         return requiredParams;
     }
-
-    public String getTask() {
-        return task;
-    }
-}
