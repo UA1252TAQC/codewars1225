ee33a6ae013cc63adaea0f118bd8709dd3959e0a
YanOleks <yanytskyi.oleksandr@gmail.com>
2024-08-14 19:51:08 +0300

    implemented task controller

--- src/main/java/org/academy/codewars/controllers/TaskController.java
+++ src/main/java/org/academy/codewars/controllers/TaskController.java
@@ -1,4 +1,19 @@
 package org.academy.codewars.controllers;
 
+import org.academy.codewars.entities.Task;
+import org.academy.codewars.services.TaskService;
+
+import java.util.List;
+
 public class TaskController {
+    private TaskService taskService;
+    public TaskController(TaskService service){
+        taskService = service;
+    }
+    public List<Task> getAll() {
+        return taskService.getAll();
+    }
+    public Task getById(int id){
+        return taskService.getById(id);
+    }
 }
