f4596c35988dc1705a6e8ce573fe8b832ad02050
Vladyslava <vldnezhnova@gmail.com>
2024-08-14 16:57:42 +0200

    Implement TaskService

--- src/main/java/org/academy/codewars/services/TaskService.java
+++ src/main/java/org/academy/codewars/services/TaskService.java
@@ -1,4 +1,33 @@
 package org.academy.codewars.services;
 
+import org.academy.codewars.entities.Author;
+import org.academy.codewars.entities.Task;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class TaskService {
+
+    public List<Task> getAll(int authorId) {
+        Author author = Author.values()[authorId];
+        List<Task> authorTasks = new ArrayList<>();
+
+        authorTasks.add(Task.TASK1);
+        authorTasks.add(Task.TASK2);
+        authorTasks.add(Task.TASK3);
+
+        return authorTasks;
+    }
+
+    public Task getById(int authorId, String taskName) {
+        Author author = Author.values()[authorId];
+
+        for (Task task : getAll(authorId)) {
+            if (task.name().equals(taskName)) {
+                return task;
+            }
+        }
+
+        throw new IllegalArgumentException("Task " + taskName + " is not associated with author " + author.getName());
+    }
 }
