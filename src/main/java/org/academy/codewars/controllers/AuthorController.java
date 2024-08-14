440a014b311a995cb8e4cdb9a0c7ae4e30fb3304
ol271176 <ol271176@gmail.com>
2024-08-14 16:56:46 +0300

    implemented AuthorController

--- src/main/java/org/academy/codewars/controllers/AuthorController.java
+++ src/main/java/org/academy/codewars/controllers/AuthorController.java
@@ -1,4 +1,26 @@
 package org.academy.codewars.controllers;
 
+import org.academy.codewars.entities.Author;
+import org.academy.codewars.services.AuthorService;
+
+import java.util.List;
+
 public class AuthorController {
+    private final AuthorService authorService;
+
+    public AuthorController(AuthorService authorService) {
+        this.authorService = authorService;
+    }
+
+    public List<Author> getAll() {
+        return authorService.getAll();
+    }
+
+    public Author getById(int id) {
+        if (id < 0) {
+            throw new IllegalArgumentException("ID must be a positive number.");
+        }
+        return authorService.getById(id);
+    }
+
 }
