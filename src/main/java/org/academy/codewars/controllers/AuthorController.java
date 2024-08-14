b4e2555fe34724fcc2483abd74c1b25a603bde65
ol271176 <ol271176@gmail.com>
2024-08-14 17:18:24 +0300

    fix parameter name

--- src/main/java/org/academy/codewars/controllers/AuthorController.java
+++ src/main/java/org/academy/codewars/controllers/AuthorController.java
@@ -13,26 +13,14 @@ public class AuthorController {
     }
 
     public List<Author> getAll() {
-        List<Author> authors = authorService.getAll();
-        if (authors == null) {
-            throw new IllegalStateException("List of Authors can't be null.");
-        }
-
-        if (authors.isEmpty()) {
-            System.out.println("Map of authors is empty");
-        }
-        return authors;
+        return authorService.getAll();
     }
 
-    public Author getByID(int id) {
+    public Author getById(int id) {
         if (id <= 0) {
             throw new IllegalArgumentException("ID must be a positive number.");
         }
-        Author author = authorService.getByID(id);
-        if (author == null) {
-            throw new IllegalArgumentException("No author with this ID was found.");
-        }
-        return author;
+        return authorService.getById(id);
     }
 
 }
