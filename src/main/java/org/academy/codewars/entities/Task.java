9de52f0b24e5ad7ef3e16b58572563c0a77c06a2
Oyne <andreoyy.litvinovv@gmail.com>
2024-08-14 21:44:15 +0300

    Task1-3 Arguments change

--- src/main/java/org/academy/codewars/entities/Task.java
+++ src/main/java/org/academy/codewars/entities/Task.java
@@ -1,41 +1,38 @@
 package org.academy.codewars.entities;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 import java.util.function.BiFunction;
-import java.util.function.Supplier;
 
 public enum Task {
     TASK1("Keep Hydrated!",
             "You get given the time in hours and you need to return the number of litres Nathan will drink, rounded to the smallest value.",
-            (author, params) -> String.valueOf(author.getEight().liters(Double.parseDouble(Arrays.toString(params)))),
-            new ArrayList<>(Collections.singletonList("Enter time(Ex: 7.32)"))),
+            (author, params) -> author.getEight().liters((Double) params.get(0)),
+            new String[]{"Enter time(Ex: 7.32)"}),
     TASK2("Volume of a Cuboid",
             "Bob needs a fast way to calculate the volume of a cuboid with three values: the length, width and height of the cuboid.",
             (author, params) ->
-                    String.valueOf(author.getEight().getVolumeOfCuboid(
-                            Double.parseDouble(params[0].toString()),
-                            Double.parseDouble(params[1].toString()),
-                            Double.parseDouble(params[2].toString()))),
-            new ArrayList<>(Arrays.asList("Enter length(Ex: 2)", "Enter width(Ex.: 4.1)", "Enter width(Ex.: 3.9)"))),
+                    author.getEight().getVolumeOfCuboid((Double) params.get(0), (Double) params.get(1), (Double) params.get(2)),
+            new String[]{"Enter length(Ex: 2)", "Enter width(Ex.: 4.1)", "Enter width(Ex.: 3.9)"}),
     TASK3("Miles per gallon to kilometers per liter",
             "Sometimes, I want to quickly be able to convert miles per imperial gallon (mpg) into kilometers per liter (kpl).",
             (author, params) ->
-                    String.valueOf(author.getEight().mpgToKPM(
-                            Float.parseFloat(Arrays.toString(params)))),
-            new ArrayList<>(Collections.singletonList("Enter mpg(Ex: 2.2)")));
+                    String.valueOf(author.getEight().mpgToKPM((Float) params.get(0))),
+            new String[]{"Enter mpg(Ex: 2.2)"});
+
     private final String title;
     private final String description;
-    private final BiFunction<Author, Object[], String> supplier;
-    private final List<String> requiredParams;
+    private final BiFunction<Author, List<Object>, ?> supplier;
+    private final String[] requiredParams;
 
-    Task(String title, String description, BiFunction<Author, Object[], String> supplier, List<String> requiredParams) {
+    Task(String title, String description, BiFunction<Author, List<Object>, ?> supplier, String[] requiredParams) {
         this.title = title;
         this.description = description;
         this.supplier = supplier;
         this.requiredParams = requiredParams;
+    }
+
+    public int getId() {
+        return this.ordinal();
     }
 
     public String getTitle() {
@@ -46,11 +43,11 @@ public enum Task {
         return description;
     }
 
-    public BiFunction<Author, Object[], String> getSupplier() {
+    public BiFunction<Author, List<Object>, ?> getSupplier() {
         return supplier;
     }
 
-    public List<String> getRequiredParams() {
+    public String[] getRequiredParams() {
         return requiredParams;
     }
 }
