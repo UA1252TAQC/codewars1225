a7330710c9cdbfbc2636f34b346cdee5001bcf64
amina <aminakrevetka@gmail.com>
2024-08-14 17:23:31 +0300

    fix enum task

--- src/main/java/org/academy/codewars/entities/Task.java
+++ src/main/java/org/academy/codewars/entities/Task.java
@@ -1,19 +1,20 @@
 package org.academy.codewars.entities;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Supplier;
 
 public enum Task {
-    TASK1("Title1", "Description1", () -> new Object(), List.of("Param1"),
-            TASK2("Title2", "Description2", () -> new Object(), List.of("Param2"),
-                    TASK3("Title3", "Description3", () -> new Object(), List.of("Param3");
+    TASK1("Title1", "Description1", () -> new Object(), new ArrayList<>()),
+    TASK2("Title2", "Description2", () -> new Object(), new ArrayList<>()),
+    TASK3("Title3", "Description3", () -> new Object(), new ArrayList<>());
 
     private final String title;
     private final String description;
     private final Supplier<Object> supplier;
     private final List<String> requiredParams;
 
-    public Task(String title, String description, Supplier<Object> supplier, List<String> requiredParams) {
+    Task(String title, String description, Supplier<Object> supplier, List<String> requiredParams) {
         this.title = title;
         this.description = description;
         this.supplier = supplier;
@@ -35,3 +36,4 @@ public enum Task {
     public List<String> getRequiredParams() {
         return requiredParams;
     }
+}
