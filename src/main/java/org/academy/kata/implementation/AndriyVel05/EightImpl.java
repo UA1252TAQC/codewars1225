08752d6d3fc411c81ffd3f47565e66b290f2f501
AndriyVel05 <andriy.velychko.work@gmail.com>
2024-08-15 11:57:07 +0300

    Task_1_Velychko

--- src/main/java/org/academy/kata/implementation/AndriyVel05/EightImpl.java
+++ src/main/java/org/academy/kata/implementation/AndriyVel05/EightImpl.java
@@ -5,7 +5,7 @@ import org.academy.kata.Eight;
 public class EightImpl implements Eight {
     @Override
     public int liters(double time) {
-        return 0;
+        return (int) Math.floor(time * 0.5)
     }
 
     @Override
