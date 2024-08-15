185efeee8da5bf16a904bff7c2f8527e5f74462c
mihail <turubarovvvv@gmail.com>
2024-08-15 16:47:00 +0300

    Fixed AuthorService

--- src/main/java/org/academy/codewars/services/AuthorService.java
+++ src/main/java/org/academy/codewars/services/AuthorService.java
@@ -1,24 +1,20 @@
 package org.academy.codewars.services;
 
-import org.academy.codewars.repositories.AuthorRepository;
 import org.academy.codewars.entities.Author;
 
 import java.util.List;
 
 public class AuthorService {
-    private final AuthorRepository authorRepository;
-
-    public AuthorService(AuthorRepository authorRepository) {
-        this.authorRepository = authorRepository;
-    }
 
     public List<Author> getAll() {
-        return authorRepository.findAll();
+        return Author.findAll();
     }
 
     public Author getById(int id) {
-        return authorRepository.findById(id)
-                .orElseThrow(() -> new RuntimeException("Author not found with id: " + id));
+        Author author = Author.findById(id);
+        if (author == null) {
+            throw new RuntimeException("Author not found with id: " + id);
+        }
+        return author;
     }
-
 }
