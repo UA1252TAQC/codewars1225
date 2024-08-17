585948b5b99990c4f665aca3d2ac0fe17cc729dc
AndriyVel05 <andriy.velychko.work@gmail.com>
2024-08-17 12:34:06 +0300

    Task_15_Velychko

--- src/main/java/org/academy/kata/Six.java
+++ src/main/java/org/academy/kata/Six.java
@@ -7,9 +7,40 @@ public interface Six {
 
     double f(double x);
 
-    double mean(String town, String strng);
+    public static double mean(String town, String strng) {
+        String[] records = strng.split("/n");
+        for (String record : records) {
+            if (record.startsWith(town + ":")) {
+                double[] rainfall = Arrays.stream(record.replace(town + ":", "").split(","))
+                        .mapToDouble(r -> Double.parseDouble(r.split(" ")[1]))
+                        .toArray();
+                return Arrays.stream(rainfall).average().orElse(-1);
+            }
+        }
 
-    double variance(String town, String strng);
+        return -1;
+    }
+
+    public static double variance(String town, String strng) {
+        double avg = mean(town, strng);
+        if (avg == -1) return -1;
+
+        String[] records = strng.split("\n");
+        for (String.record : records) {
+            if (record.startsWith(town + ":")) {
+                double[] rainfall = Arrays.stream(record.replace(town + ":", "").split(","))
+                        .mapToDouble(r -> Double.parseDouble(r.split(" ")[1]))
+                        .toArray();
+                double variance = Arrays.stream(rainfall)
+                        .map(r -> Math.pow(r - avg, 2))
+f                       .average()
+                        .orElse(-1);
+                return variance;
+            }
+        }
+
+        return -1;
+    }
 
     String nbaCup(String resultSheet, String toFind);
 
