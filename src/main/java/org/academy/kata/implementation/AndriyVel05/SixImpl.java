bfb76aaeaad6bba20db0bb109b26e377d9a8a14c
AndriyVel05 <andriy.velychko.work@gmail.com>
2024-08-17 12:56:08 +0300

    Task_16_Velychko

--- src/main/java/org/academy/kata/implementation/AndriyVel05/SixImpl.java
+++ src/main/java/org/academy/kata/implementation/AndriyVel05/SixImpl.java
@@ -1,6 +1,8 @@
 package org.academy.kata.implementation.AndriyVel05;
 
 import org.academy.kata.Six;
+import java.util.HashMap;
+import java.util.Map;
 
 public class SixImpl implements Six {
     @Override
@@ -34,7 +36,26 @@ public class SixImpl implements Six {
     }
 
     @Override
-    public String stockSummary(String[] lstOfArt, String[] lstOf1stLetter) {
-        return "";
+    public static String stockSummary(String[] lstOfArt, String[] lstOf1stLetter) {
+        if (lstOfArt.length == 0 || lstOfArt.length == 0) return "";
+
+        Map<Strimg, Integer> categoryMap = new HashMap<>();
+        for (String letter : lstOf1stLetter) {
+            categoryMap.put(letter, 0);
+        }
+
+        for (String art : lstOfArt) {
+            String category = art.substring(0,1);
+            int quantity = Integer.parseInt(art.split("")[1]);
+            if (categoryMap.containsKey(category)) {
+                categoryMap.put(category, categoryMap.get(category) + quantity);
+            }
+        }
+
+        StringBuilder result = new StringBuilder();
+        for (String letter : lstOf1stLetter) {
+            result.append("(").append(letter).append(" : ").append(categoryMap.get(letter)).append(") - ");
+        }
+        return result.substring(0, result.length() - 3);
     }
 }
