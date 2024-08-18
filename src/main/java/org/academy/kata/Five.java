2e97f054913f690da9ab54d85ad647c6f1369f3f
AndriyVel05 <andriy.velychko.work@gmail.com>
2024-08-18 12:23:00 +0300

    Task_19_Velychko

--- src/main/java/org/academy/kata/Five.java
+++ src/main/java/org/academy/kata/Five.java
@@ -7,7 +7,21 @@ public interface Five {
 
     int zeros(int n);
 
-    BigInteger perimeter(BigInteger n);
+    public BigInteger perimeter(BigInteger n) {
+        BigInteger a = BigInteger.ONE;
+        BigInteger b = BigInteger.ONE;
+        BigInteger sum = a.add(b);
+
+        for (BigInteger i = BigInteger.valueOf(2); i.compareTo(n) <= 0; i = i.add(BigInteger.ONE)) {
+            BigInteger next = a.add(b);
+            sum = sum.add(next);
+            a = b;
+            b = next;
+        }
+
+        return sum.multiply(BigInteger.valueOf(4));
+
+    }
 
     double solve(double m);
 
