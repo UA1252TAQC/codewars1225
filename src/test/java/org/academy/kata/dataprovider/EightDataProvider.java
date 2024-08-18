79cf37c2b132343e3bf56d1b034d80b2ab0d7f1e
ol271176 <ol271176@gmail.com>
2024-08-18 14:49:30 +0300

    Added unit test for stringToNumber method

--- src/test/java/org/academy/kata/dataprovider/EightDataProvider.java
+++ src/test/java/org/academy/kata/dataprovider/EightDataProvider.java
@@ -1,5 +1,6 @@
 package org.academy.kata.dataprovider;
 
+import org.testng.Assert;
 import org.testng.annotations.DataProvider;
 
 import java.util.ArrayList;
@@ -14,6 +15,17 @@ public class EightDataProvider extends AbstractDataProvider {
         inputs.add(new Object[]{80, 40});
         inputs.add(new Object[]{1600.20, 800});
         inputs.add(new Object[]{14.64, 7});
+
+        return combineData(inputs, EIGHT);
+    }
+
+    @DataProvider(name = "data-stringToNumber")
+    public static Iterator<Object[]> dpStringToNumber(){
+        List<Object[]> inputs = new ArrayList<>();
+        inputs.add(new Object[]{"1234", 1234});
+        inputs.add(new Object[]{"605", 605});
+        inputs.add(new Object[]{"1405", 1405});
+        inputs.add(new Object[]{"-7", -7});
 
         return combineData(inputs, EIGHT);
     }
