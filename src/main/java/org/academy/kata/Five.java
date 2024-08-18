bb6dc59fbcd6e6b053f7e8c652eb9b827ccc737d
AndriyVel05 <andriy.velychko.work@gmail.com>
2024-08-18 12:07:27 +0300

    Task_17_Velychko

--- src/main/java/org/academy/kata/Five.java
+++ src/main/java/org/academy/kata/Five.java
@@ -3,9 +3,33 @@ package org.academy.kata;
 import java.math.BigInteger;
 
 public interface Five {
-    long[] gap(int g, long m, long n);
+    public class PrimeGap {
+        public static long[] gap(int g, long m, long n) {
+            long prevPrime = 0;
 
-    int zeros(int n);
+            for (long i = m; i <= n; i++) {
+                if (isPrime(i)) {
+                    if (prevPrime != 0 && i - prevPrime == g) {
+                        return new long[]{prevPrime, i};
+                    }
+                    prevPrime = i;
+                }
+            }
+            return null;
+        }
+
+        private static boolean isPrime(long num) {
+            if (num < 2) return false;
+            for (long i = 2; i * i <= num; i++) {
+                if (num % i == 0) return false;
+            }
+            return true;
+        }
+
+    }
+
+
+        int zeros(int n);
 
     BigInteger perimeter(BigInteger n);
 
