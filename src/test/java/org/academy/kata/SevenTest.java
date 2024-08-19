c0b21c49d91c277514c98a645d3bff00165fcb3c
mihail <turubarovvvv@gmail.com>
2024-08-19 12:40:15 +0300

    Made test for task NewAvg

--- src/test/java/org/academy/kata/SevenTest.java
+++ src/test/java/org/academy/kata/SevenTest.java
@@ -1,13 +1,17 @@
 package org.academy.kata;
 
+import org.academy.kata.dataprovider.EightDataProvider;
+import org.academy.kata.dataprovider.SevenDataProvider;
 import org.testng.annotations.Test;
 
 import static org.testng.Assert.*;
 
-public class SevenTest {
+public class SevenTest extends SevenDataProvider {
 
-    @Test
-    public void testNewAvg() {
+    @Test(dataProvider = "data-NewAvg")
+    public void testNewAvg(Seven seven, double[] arr, double navg, long expected) {
+        long  actual = seven.newAvg(arr, navg);
+        assertEquals(actual, expected);
     }
 
     @Test
---
+++ src/test/java/org/academy/kata/dataprovider/SevenDataProvider.java
@@ -0,0 +1,21 @@
+package org.academy.kata.dataprovider;
+
+import org.testng.annotations.DataProvider;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+
+public class SevenDataProvider extends AbstractDataProvider {
+
+  @DataProvider(name = "data-NewAvg")
+  public static Iterator<Object[]> dpNewAvg() {
+    List<Object[]> inputs = new ArrayList<>();
+    inputs.add(new Object[]{ new double[]{14, 30, 5, 7, 9, 11, 15}, 30, 149L});
+    inputs.add(new Object[]{ new double[]{14, 30, 5, 7, 9, 11, 15}, 92, 645L});
+
+    return combineData(inputs, SEVEN);
+  }
+}
+
