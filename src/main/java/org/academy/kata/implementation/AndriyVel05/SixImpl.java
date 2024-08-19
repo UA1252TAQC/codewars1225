00c914108764f9ba1485d14da2804bd65e7c0d89
AndriyVel05 <andriy.velychko.work@gmail.com>
2024-08-19 16:01:20 +0300

    Task_15_Velychko

--- src/main/java/org/academy/kata/implementation/AndriyVel05/SixImpl.java
+++ src/main/java/org/academy/kata/implementation/AndriyVel05/SixImpl.java
@@ -1,5 +1,4 @@
 package org.academy.kata.implementation.AndriyVel05;
-
 import org.academy.kata.Six;
 
 public class SixImpl implements Six {
@@ -35,6 +34,25 @@ public class SixImpl implements Six {
 
     @Override
     public String stockSummary(String[] lstOfArt, String[] lstOf1stLetter) {
-        return "";
+        if (lstOfArt.length == 0 || lstOf1stLetter.length == 0) {
+            return "";
+        }
+
+        StringBuilder result = new StringBuilder();
+        for (String category : lstOf1stLetter) {
+            int sum = 0;
+            for (String art : lstOfArt) {
+                if (art.startsWith(category)) {
+                    sum += Integer.parseInt(art.split(" ")[1]);
+                }
+            }
+            if (result.length() > 0) {
+                result.append(" - ");
+            }
+            result.append("(").append(category).append(" : ").append(sum).append(")");
+        }
+
+        return result.toString();
     }
+
 }
