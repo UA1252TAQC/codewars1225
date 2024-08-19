92ccebbd9086867576aa8fcfd8e6eaee61f07d65
AndriyVel05 <andriy.velychko.work@gmail.com>
2024-08-19 14:59:16 +0300

    Task_17_Velychko

--- src/main/java/org/academy/kata/implementation/AndriyVel05/FiveImpl.java
+++ src/main/java/org/academy/kata/implementation/AndriyVel05/FiveImpl.java
@@ -1,15 +1,35 @@
 package org.academy.kata.implementation.AndriyVel05;
 
 import org.academy.kata.Five;
-
 import java.math.BigInteger;
 
 public class FiveImpl implements Five {
     @Override
     public long[] gap(int g, long m, long n) {
-        return new long[0];
+        long prevPrime = 0;
+
+        for (long i = m; i <= n; i++) {
+            if (isPrime(i)) {
+                if (prevPrime != 0 && i - prevPrime == g) {
+                    return new long[] {prevPrime, i};
+                }
+                prevPrime = i;
+            }
+        }
+        return new long[0]; // return an empty array if no such gap exists
     }
 
+    private boolean isPrime(long num) {
+        if (num <= 1) return false;
+        if (num == 2 || num == 3) return true;
+        if (num % 2 == 0 || num % 3 == 0) return false;
+        for (long i = 5; i * i <= num; i += 6) {
+            if (num % i == 0 || num % (i + 2) == 0) return false;
+        }
+        return true;
+    }
+
+    // Other methods
     @Override
     public int zeros(int n) {
         return 0;
@@ -30,3 +50,4 @@ public class FiveImpl implements Five {
         return new long[0];
     }
 }
+
