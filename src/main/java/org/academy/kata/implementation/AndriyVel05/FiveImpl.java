c466b2d75efbad3f5886dd8b080c88061dbd856a
AndriyVel05 <andriy.velychko.work@gmail.com>
2024-08-19 15:18:41 +0300

    Task_21_Velychko

--- src/main/java/org/academy/kata/implementation/AndriyVel05/FiveImpl.java
+++ src/main/java/org/academy/kata/implementation/AndriyVel05/FiveImpl.java
@@ -27,6 +27,27 @@ public class FiveImpl implements Five {
 
     @Override
     public long[] smallest(long n) {
-        return new long[0];
+        String numStr = String.valueOf(n);
+        int len = numStr.length();
+        long smallestNum = n;
+        int[] result = {0, 0, 0}; // {smallestNum, i, j}
+
+        for (int i = 0; i < len; i++) {
+            for (int j = 0; j < len; j++) {
+                StringBuilder sb = new StringBuilder(numStr);
+                char ch = sb.charAt(i);
+                sb.deleteCharAt(i);
+                sb.insert(j, ch);
+                long newNum = Long.parseLong(sb.toString());
+
+                if (newNum < smallestNum || (newNum == smallestNum && i < result[1])) {
+                    smallestNum = newNum;
+                    result[0] = (int) smallestNum;
+                    result[1] = i;
+                    result[2] = j;
+                }
+            }
+        }
+        return new long[]{smallestNum, result[1], result[2]};
     }
 }
