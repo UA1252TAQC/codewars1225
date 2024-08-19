db5c118b12d78d7cbbd746380c0713c244207160
AndriyVel05 <andriy.velychko.work@gmail.com>
2024-08-19 13:18:29 +0300

    Task_6_Velychko

--- src/main/java/org/academy/kata/implementation/AndriyVel05/EightImpl.java
+++ src/main/java/org/academy/kata/implementation/AndriyVel05/EightImpl.java
@@ -59,7 +59,7 @@ public class EightImpl implements Eight {
 
     @Override
     public int stringToNumber(String str) {
-        return Integer.parseInt(str); // solved
+        return Integer.parseInt(str);
     }
 
     @Override
