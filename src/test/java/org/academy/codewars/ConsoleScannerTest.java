4e6245825c172738330a259acad187cbc3638321
Vladyslava <vldnezhnova@gmail.com>
2024-08-19 14:33:28 +0200

    Added tests ReadBigIntegerValidData

--- src/test/java/org/academy/codewars/ConsoleScannerTest.java
+++ src/test/java/org/academy/codewars/ConsoleScannerTest.java
@@ -4,103 +4,121 @@ import static org.testng.Assert.assertEquals;
 
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
+import java.math.BigInteger;
 import java.util.Scanner;
-
 import org.testng.annotations.Test;
 
 public class ConsoleScannerTest {
 
-    @Test
-    public void testReadInt() {
-        System.setIn(new ByteArrayInputStream("25".getBytes()));
-        InputStream inputStream = System.in;
-        ConsoleScanner consoleScanner = new ConsoleScanner(new Scanner(inputStream));
-        int actual = consoleScanner.readInt("test");
-        assertEquals(actual, 25);
+  @Test
+  public void testReadInt() {
+    System.setIn(new ByteArrayInputStream("25".getBytes()));
+    InputStream inputStream = System.in;
+    ConsoleScanner consoleScanner = new ConsoleScanner(new Scanner(inputStream));
+    int actual = consoleScanner.readInt("test");
+    assertEquals(actual, 25);
 
-    }
+  }
 
-    @Test
-    public void testReadFloat() {
-    }
+  @Test
+  public void testReadFloat() {
+  }
 
-    @Test
-    public void testReadLong() {
-    }
+  @Test
+  public void testReadLong() {
+  }
 
-    @Test
-    public void testReadDouble() {
-    }
+  @Test
+  public void testReadDouble() {
+  }
 
-    @Test
-    public void testReadBigInteger() {
-    }
+  @Test
+  public void testReadBigIntegerValidData() {
+    String testData = "12345678901234567890\n";
+    BigInteger expected = new BigInteger("12345678901234567890");
+    System.setIn(new ByteArrayInputStream(testData.getBytes()));
+    InputStream inputStream = System.in;
+    ConsoleScanner consoleScanner = new ConsoleScanner(new Scanner(inputStream));
+    BigInteger actual = consoleScanner.readBigInteger("test BigInteger");
+    assertEquals(actual, expected);
+  }
 
-    @Test
-    public void testReadIntArray() {
-    }
+  @Test
+  public void testReadBigIntegerInvalidData() {
+    String testData = "invalid\nsecondInvalid\n12345678901234567890\n";
+    BigInteger expected = new BigInteger("12345678901234567890");
+    System.setIn(new ByteArrayInputStream(testData.getBytes()));
+    InputStream inputStream = System.in;
+    ConsoleScanner consoleScanner = new ConsoleScanner(new Scanner(inputStream));
+    BigInteger actual = consoleScanner.readBigInteger("test BigInteger");
+    assertEquals(actual, expected);
+  }
 
-    @Test
-    public void testReadDoubleArrayValidData() {
-        String testData = "3\n1.1\n2.2\n3.3\n";
-        double[] expected = new double[]{1.1, 2.2, 3.3};
-        System.setIn(new ByteArrayInputStream(testData.getBytes()));
-        InputStream inputStream = System.in;
-        ConsoleScanner consoleScanner = new ConsoleScanner(new Scanner(inputStream));
-        double[] actual = consoleScanner.readDoubleArray("test array");
-        assertEquals(actual, expected);
-    }
+  @Test
+  public void testReadIntArray() {
+  }
 
-    @Test
-    public void testReadDoubleArrayInvalidData() {
-        String testData = "invalid\n-3\n3\n1.1\n2.2\n3.3\n";
-        double[] expected = new double[]{1.1, 2.2, 3.3};
-        System.setIn(new ByteArrayInputStream(testData.getBytes()));
-        InputStream inputStream = System.in;
-        ConsoleScanner consoleScanner = new ConsoleScanner(new Scanner(inputStream));
-        double[] actual = consoleScanner.readDoubleArray("test array");
-        assertEquals(actual, expected);
-    }
+  @Test
+  public void testReadDoubleArrayValidData() {
+    String testData = "3\n1.1\n2.2\n3.3\n";
+    double[] expected = new double[]{1.1, 2.2, 3.3};
+    System.setIn(new ByteArrayInputStream(testData.getBytes()));
+    InputStream inputStream = System.in;
+    ConsoleScanner consoleScanner = new ConsoleScanner(new Scanner(inputStream));
+    double[] actual = consoleScanner.readDoubleArray("test array");
+    assertEquals(actual, expected);
+  }
 
-    @Test
-    public void testReadString() {
-    }
+  @Test
+  public void testReadDoubleArrayInvalidData() {
+    String testData = "invalid\n-3\n3\n1.1\n2.2\n3.3\n";
+    double[] expected = new double[]{1.1, 2.2, 3.3};
+    System.setIn(new ByteArrayInputStream(testData.getBytes()));
+    InputStream inputStream = System.in;
+    ConsoleScanner consoleScanner = new ConsoleScanner(new Scanner(inputStream));
+    double[] actual = consoleScanner.readDoubleArray("test array");
+    assertEquals(actual, expected);
+  }
 
-    @Test
-    public void testReadStringArrayValidData() {
-        String testData =
-                """
-                        t
-                        -3
-                        3
-                        First arg
-                        Second arg
-                        Third arg
-                        """;
-        String[] expected = new String[]{"First arg", "Second arg", "Third arg"};
-        System.setIn(new ByteArrayInputStream(testData.getBytes()));
-        InputStream inputStream = System.in;
-        ConsoleScanner consoleScanner = new ConsoleScanner(new Scanner(inputStream));
-        String[] actual = consoleScanner.readStringArray("test");
-        assertEquals(actual, expected);
-    }
+  @Test
+  public void testReadString() {
+  }
 
-    @Test
-    public void testReadStringArrayInvalidData() {
-        String testData =
-                """
-                         test
-                         -3
-                         3
-                         First arg\s
-                         Second arg
-                         Third arg
-                        \s""";
-        String[] expected = new String[]{"First arg", "Second arg", "Third arg"};
-        System.setIn(new ByteArrayInputStream(testData.getBytes()));
-        InputStream inputStream = System.in;
-        ConsoleScanner consoleScanner = new ConsoleScanner(new Scanner(inputStream));
-        String[] actual = consoleScanner.readStringArray("test");
-        assertEquals(actual, expected);
-    }
+  @Test
+  public void testReadStringArrayValidData() {
+    String testData =
+        """
+            t
+            -3
+            3
+            First arg
+            Second arg
+            Third arg
+            """;
+    String[] expected = new String[]{"First arg", "Second arg", "Third arg"};
+    System.setIn(new ByteArrayInputStream(testData.getBytes()));
+    InputStream inputStream = System.in;
+    ConsoleScanner consoleScanner = new ConsoleScanner(new Scanner(inputStream));
+    String[] actual = consoleScanner.readStringArray("test");
+    assertEquals(actual, expected);
+  }
+
+  @Test
+  public void testReadStringArrayInvalidData() {
+    String testData =
+        """
+             test
+             -3
+             3
+             First arg\s
+             Second arg
+             Third arg
+            \s""";
+    String[] expected = new String[]{"First arg", "Second arg", "Third arg"};
+    System.setIn(new ByteArrayInputStream(testData.getBytes()));
+    InputStream inputStream = System.in;
+    ConsoleScanner consoleScanner = new ConsoleScanner(new Scanner(inputStream));
+    String[] actual = consoleScanner.readStringArray("test");
+    assertEquals(actual, expected);
+  }
 }
