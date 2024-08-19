89d7ff189762e96a19261989be54a035b9a4710f
AndriyVel05 <andriy.velychko.work@gmail.com>
2024-08-19 11:32:50 +0300

    Task_21_Velychko

--- src/main/java/org/academy/kata/Five.java
+++ src/main/java/org/academy/kata/Five.java
@@ -11,6 +11,29 @@ public interface Five {
 
     double solve(double m);
 
-    long[] smallest(long n);
+    public long[] smallest(long n) {
+        String numStr = Long.toString(n);
+        long minVal = n;
+        int minL = 0, minJ = 0;
+
+        for (int i = 0; i < numStr.length(); i++) {
+            char ch = numStr.charAt(i);
+            StringBuilder sb = new StringBuilder(numStr);
+            sb.deleteCharAt(i);
+
+            for (int j = 0; j <= sb.length(); j++) {
+                StringBuilder newSb = new StringBuilder(sb);
+                newSb.insert(j, ch);
+                long newVal = Long.parseLong(newSb.toString());
+
+                if (newVal < minVal) {
+                    minVal = newVal;
+                    minL = i;
+                    minJ = j;
+                }
+            }
+        }
+        return new long[]{minVal, minL, minJ};
+    }
 
 }
