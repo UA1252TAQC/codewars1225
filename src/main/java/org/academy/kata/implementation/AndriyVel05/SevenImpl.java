eb9a2c702b5e752c3db1fb7c0f98d35aec7c82dd
AndriyVel05 <andriy.velychko.work@gmail.com>
2024-08-20 11:34:41 +0300

    Tasks_8_7_kyu_fixed

--- src/main/java/org/academy/kata/implementation/AndriyVel05/SevenImpl.java
+++ src/main/java/org/academy/kata/implementation/AndriyVel05/SevenImpl.java
@@ -27,7 +27,7 @@ public class SevenImpl extends Base implements Seven {
         }
         double sum = 0.0;
         for (int i = 0; i < n; i++) {
-            sum += 1.0 / (3 * i + 1); // Виправлений розрахунок
+            sum += 1.0 / (3 * i + 1);
         }
         return String.format("%.2f", sum);
     }
