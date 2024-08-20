b42c7e63440b45ac311ac77e778ca0e4ce1bc696
AndriyVel05 <andriy.velychko.work@gmail.com>
2024-08-20 10:52:41 +0300

    Test_SeriesSum_Completed

--- src/test/java/org/academy/kata/dataprovider/SevenDataProvider.java
+++ src/test/java/org/academy/kata/dataprovider/SevenDataProvider.java
@@ -1,7 +1,5 @@
 package org.academy.kata.dataprovider;
-
 import org.testng.annotations.DataProvider;
-
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
@@ -17,4 +15,16 @@ public class SevenDataProvider extends AbstractDataProvider {
 
         return combineData(inputs, SEVEN);
     }
+
+    @DataProvider(name = "data-SeriesSum")
+    public static Iterator<Object[]> dpSeriesSum() {
+        List<Object[]> inputs = new ArrayList<>();
+        inputs.add(new Object[]{0, "0.00"});
+        inputs.add(new Object[]{1, "1.00"});
+        inputs.add(new Object[]{2, "1.25"});
+        inputs.add(new Object[]{5, "1.57"});
+
+        return combineData(inputs, SEVEN);
+    }
+
 }
