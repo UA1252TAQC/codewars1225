a377d00cf4da4d4033d554eca96452d933eee34d
s.bekberov <s.bekberov@gmail.com>
2024-08-21 18:15:56 +0300

    Fixed tests

--- src/test/java/org/academy/codewars/dataproviders/ConsoleScannerDataProvider.java
+++ src/test/java/org/academy/codewars/dataproviders/ConsoleScannerDataProvider.java
@@ -12,12 +12,12 @@ public class ConsoleScannerDataProvider {
     public Iterator<Object[]> testReadStringArray() {
         List<Object[]> inputs = new ArrayList<>();
         String param = "param";
-        String expectedOutput1 = "Enter a size for array > 0 test (int): Enter elements String[]:" + System.lineSeparator() +
-                "Element_1 => Enter a new string : Element_2 => Enter a new string : Element_3 => Enter a new string : ";
+        String expectedOutput1 = "Enter a size for array > 0 " + param +  " (int): Enter elements String[]:" + System.lineSeparator() +
+                "Element_1 => Enter a new string: Element_2 => Enter a new string: Element_3 => Enter a new string: ";
         String expectedOutput2 =
-                "Enter a size for array > 0 test (int): Invalid data format. An integer is expected." + System.lineSeparator() +
-                "Enter a size for array > 0 test (int): Enter a size for array > 0 test (int): Enter elements String[]:" + System.lineSeparator() +
-                "Element_1 => Enter a new string : Element_2 => Enter a new string : Element_3 => Enter a new string : ";
+                "Enter a size for array > 0 " + param +  " (int): Invalid data format. An integer is expected." + System.lineSeparator() +
+                "Enter a size for array > 0 " + param + " (int): Enter a size for array > 0 " + param + " (int): Enter elements String[]:" + System.lineSeparator() +
+                "Element_1 => Enter a new string: Element_2 => Enter a new string: Element_3 => Enter a new string: ";
 
         inputs.add(new Object[]{param, "3\nFirst arg \nSecond arg\nThird arg\n ", new String[]{"First arg", "Second arg", "Third arg"}, expectedOutput1});
         inputs.add(new Object[]{param, "test\n-3\n3\nFirst arg \nSecond arg\nThird arg\n ", new String[]{"First arg", "Second arg", "Third arg"}, expectedOutput2});
